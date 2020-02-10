c3155ad61ccd87f4f4915d22627383cf31b3fee9
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-02-11 01:22:39 +0800

    Update VKeyboard.py

--- VKeyboard.py
+++ VKeyboard.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 class VKeyboard(tk.Toplevel):
     INSTANCE = None
-    def __init__(self):
+    def __init__(self, parent):
      # here goes the settings for the Toplevel
         super().__init__(parent)
         self.configure(background="cornflowerblue")
