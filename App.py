6823fb93968dad46b3883751980e9659db67c0b3
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-02-11 08:46:49 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,21 +1,49 @@
-if __name__ == '__main__':
-    root = tk.Tk()
-    root.title('Test Keyboard')
-
-    label = tk.Label(root, text='Test Keyboard')
-    label.grid(row=0, column=0, columnspan=2)
-
-    entry1 = tk.Entry(root)
-    entry1.grid(row=1, column=0, sticky='news')
-    entry1.bind("<FocusIn>", lambda event: create(root, entry1))
+import tkinter as tk
+from VKeyboard import VKeyboard
 
 
-    entry2 = tk.Entry(root)
-    entry2.grid(row=2, column=0, sticky='news')
-    entry2.bind("<FocusIn>", lambda event: create(root, entry2))
-    
-    text1 = tk.Text(root)
-    text1.grid(row=3, column=0, sticky='news')
-    text1.bind("<FocusIn>", lambda event: create(root, text1))
-    
-    root.mainloop()
+class LabelEntry(tk.Frame):
+    def __init__(self, parent, **kwargs):
+        text = kwargs.pop('text', 'Unknown')
+        super().__init__(parent, **kwargs)
+
+        self.variable = tk.StringVar(self)
+        pady = 5
+        tk.Label(self, text='{}\t:'.format(text), relief="ridge", bg="white").grid(row=0, column=0, padx=5, pady=pady)
+        self.entry = tk.Entry(self, textvariable=self.variable, bg="white", width="45")
+        self.entry.grid(row=0, column=1, pady=pady)
+
+
+class Form1(tk.Frame):
+    def __init__(self, parent, fields, **kwargs):
+        super().__init__(parent, **kwargs)
+
+        self.fields = dict.fromkeys(fields, None)
+
+        for row, text in enumerate(fields, 1):
+            self.fields[text] = LabelEntry(self, text=text)
+            self.fields[text].grid(row=row, column=0, pady=0)
+
+    def get(self):
+        return {key: item.entry.get() for key, item in self.fields.items()}
+
+
+class Kiosk(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Thesis")
+
+        VKeyboard(self)
+
+        form1 = Form1(self, ("First Name", "Middle Name", "Last Name"))
+        form1.grid(row=1, column=0)
+
+        vbtn = tk.Button(self, text="Proceed to Step 2", bg="white", command=lambda: self.registered(form1.get()))
+        vbtn.grid(row=2, column=0, pady=5)
+
+    def registered(self, data):
+        print('registered:{}'.format(data))
+
+
+if __name__ == "__main__":
+    Kiosk().mainloop()
