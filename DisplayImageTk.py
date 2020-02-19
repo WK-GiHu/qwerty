613fec7c663d59d0284b0bbcf1a57595c697845c
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-02-19 21:34:10 +0800

    Update DisplayImageTk.py

--- DisplayImageTk.py
+++ DisplayImageTk.py
@@ -3,6 +3,31 @@ import tkinter as tk
 import pymysql
 import io
 
+def convertToBinaryData(filename):
+    # Convert digital data to binary format
+    with open(filename, 'rb') as file:
+        binaryData = file.read()
+    return binaryData
+
+def insertBLOB(emp_id, name, photo):
+    print("Inserting BLOB into python_employee table")
+    db = pymysql.connect(host='localhost',user='root', password='', db = 'test')
+
+    cursor = db.cursor()
+    sql_insert_blob_query = """ INSERT INTO testing
+                      (ID, Name, Image) VALUES (%s,%s,%s)"""
+
+    empPicture = convertToBinaryData(photo)
+
+    # Convert data into tuple format
+    insert_blob_tuple = (emp_id, name, empPicture)
+    result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+    db.commit()
+    print("Image and file inserted successfully as a BLOB into python_employee table", result)
+
+   
+insertBLOB(1, "Justin", "saved_img-final.jpg")
+
 db = pymysql.connect(host='localhost',user='root', password='', db = 'test')
 cursor = db.cursor()
 
