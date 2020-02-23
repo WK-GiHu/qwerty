9a11f4b58622a2cfecef58415b6f2356b50e9897
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-02-23 19:50:42 +0800

    Update VKeyboard.py

--- VKeyboard.py
+++ VKeyboard.py
@@ -1,4 +1,17 @@
 import tkinter as tk
+import threading
+
+class SplashThread(threading.Thread):
+    def __init__(self, app, callback):
+        super().__init__()
+        self.app = app
+        self.app.bind('<<IDLE>>', callback)
+        self.start()
+
+    def on_idle(self):        
+        if event.state == 0:
+            self._counter = self.timeout
+            
 
 
 class VKeyboard(tk.Toplevel):
@@ -10,12 +23,14 @@ class VKeyboard(tk.Toplevel):
         super().withdraw()
         
         self.configure(background="cornflowerblue")
-        #self.geometry("+420+820")
-        self.geometry("+0+283")
+        self.geometry("+160+650")
+        #self.geometry("+0+283")
         self.wm_attributes("-alpha", 0.7)
         self.wm_attributes("-type", 'toolwindow')
         self.wm_overrideredirect(boolean=True)
         print(self.wm_geometry())
+        
+        self.event_generate('<<IDLE>>', state =0, when = 'tail')
         
         self.create()
         self.uppercase = False
@@ -35,6 +50,8 @@ class VKeyboard(tk.Toplevel):
             
             if w_class_name in ('Entry', 'TCombobox'):
                 if self.state() == 'withdrawn':
+                    self.geometry("+160+650")
+                    #self.geometry("+0+283")
                     self.deiconify()
                 
                 self.entry = w
@@ -47,7 +64,7 @@ class VKeyboard(tk.Toplevel):
         alphabets = [
             ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
             ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', "\\"],
-            ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'Enter'],
+            ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'k', ';', "'", 'Enter'],
             ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Shift'],
             ['Space']
         ]
@@ -56,19 +73,22 @@ class VKeyboard(tk.Toplevel):
             x = 0
             for text in row:
                 if text in ('Enter', 'Shift'):
-                    width = 18
+                    width = 26
                     columnspan = 2
+                    height = 3
                 elif text == 'Space':
-                    width = 124
+                    width = 200
                     columnspan = 16
+                    height = 3
                 elif text in ('Backspace', '\\', 'Tab', '`', 'Caps Lock'):
+                    width = 12
+                    columnspan = 1
+                    height = 3
+                else:
                     width = 10
                     columnspan = 1
-                else:
-                    width = 4
-                    columnspan = 1
-                
-                _btn = tk.Button(self, text=text, width=width, padx=3, pady=3, bd=12, bg="black", fg="white")
+                    height = 3
+                _btn = tk.Button(self, text=text, width=width, height=height, padx=3, pady=3, bd=12, bg="black", fg="white")
                 _btn.grid(row=y, column=x, columnspan=columnspan)
                 _btn.bind('<Button-1>', self.select)
                 
