ec0572be690f86aac890d3d0afdf5998914a548e
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-02-26 21:28:48 +0800

    Update Kiosk_Main.py

--- Kiosk_Main.py
+++ Kiosk_Main.py
@@ -30,15 +30,23 @@ class FingerprintThread(threading.Thread):
             print('Exception message: ' + str(e))
         
         print('Currently used templates: ' + str(f.getTemplateCount()) +'/'+ str(f.getStorageCapacity()))
+        retry = 0
         while True:
-        
             print('Waiting for finger...')
+            try:
+                while ( f.readImage() == False ):
+                    time.sleep(1)
+                    pass
+                break
+            except Exception as e:
+                 print('PyFingerprint:{}, try {} of 3'.format(e, retry))
+                 if retry == 3:
+                     raise Exception('PyFingerprint: Failed to read image 3 times, exiting.')
 
-            ## Wait that finger is read
-            while ( f.readImage() == False ):
-                pass
-
-                ## Converts read image to characteristics and stores it in charbuffer 1
+                 # delay 2 seconds before next try
+                 time.sleep(2)
+            
+            ## Converts read image to characteristics and stores it in charbuffer 1
             f.convertImage(0x01)
 
                 ## Searchs template
@@ -60,100 +68,6 @@ class FingerprintThread(threading.Thread):
                     self.app.event_generate('<<GRANT_ACCESS>>', state = 2, when='tail')
                 else:
                     messagebox.showerror("Warning!","Your fingerprint is not yet registered!")
-
-    
-class RFIDThread(threading.Thread):
-    def __init__(self, app, callback):
-        super().__init__(daemon = True)
-        result = None
-        self.app = app
-        self.app.bind('<<GRANT_ACCESS>>', callback)
-        self.start()
-    
-    def run(self):
-        self.db = pymysql.connect(host = "192.168.1.19",port = 3306, user = "root",passwd = "justin",db= "thesis_db")
-        self.cursor = self.db.cursor()
-        self.db.autocommit(True)
-        self.reader = SimpleMFRC522()
-        while True:
-            self.id, text = self.reader.read()
-            self.cursor.execute("SELECT * FROM residents_admin WHERE RFID = %s",str(self.id))
-            RFIDThread.result = self.cursor.fetchone()
-            print(RFIDThread.result)
-            if RFIDThread.result: 
-                messagebox.showinfo("Success!", "Welcome")
-                self.app.event_generate('<<GRANT_ACCESS>>', state = 11, when='tail')
-            else:
-                self.cursor.execute("SELECT * FROM residents_db WHERE RFID = %s", str(self.id))
-                RFIDThread.result = self.cursor.fetchone()
-                print(RFIDThread.result)
-                if RFIDThread.result: 
-                    messagebox.showinfo("Success!", "Welcome")
-                    self.app.event_generate('<<GRANT_ACCESS>>', state = 12, when='tail')
-                else:
-                    messagebox.showerror("Warning!","Your RFID card is not yet registered!")
-
-
-class SplashScreen(tk.Toplevel):
-    def __init__(self, parent):
-        super().__init__(parent)
-        self.attributes("-fullscreen", True)
-        self.funcid = self.bind('<Button-1>', self.on_button_clicked)
-        
-        
-        label = Label(self, text = "NO ANNOUNCEMENT YET!!!", bg = "white")
-        label.pack(fill = "both", expand =1)
-        
-        
-    def on_button_clicked(self, event):
-    
-        print('SplashScreen.on_button_click')
-        self.unbind('<Button-1>', funcid=self.funcid)
-        self.master.event_generate('<<IDLE>>', state = 1, when='tail')
-        self.destroy()
-         
-        
-class IdleCounter(threading.Thread):
-    label = 'idle'
-
-    def __init__(self, app):
-        super().__init__(daemon=True)
-        self.app = app
-
-        self.timeout = 20
-        self._counter = self.timeout
-
-        self.start()
-        self.app.bind('<<IDLE>>', self.on_idle)
-
-    def run(self):
-        print('run()'.format())
-        
-        while True:
-            if self._counter > 0:
-                self._counter -= 1
-                
-                # For testing only
-                print('\t{}'.format(self._counter))
-                IdleCounter.label = 'timeout in {} sec.'.format(self._counter)
-                
-                if self._counter == 0:
-                    # For testing only
-                    IdleCounter.label = 'idle, click to reset'
-                    
-                    self.app.event_generate('<<TIMEOUT>>', when='tail')
-                    
-            time.sleep(1)
-                    
-        print('IdleCounter terminated'.format())
-        
-    def on_idle(self, event):
-        print('IdleCounter.on_idle(state={})'.format(event.state))
-        if event.state == 1:
-            self._counter = self.timeout
-        elif self._counter > 0 and event.state == 0:
-            self._counter = self.timeout
-
     
 class Kiosk(tk.Tk):
     def __init__(self):
@@ -196,6 +110,3 @@ class Kiosk(tk.Tk):
         self.configure(bg="white")    
         self.geometry("{}x{}".format(self.ws, self.hs))
         self.T_printer = Usb(0x0fe6, 0x811e, 98, 0x82, 0x02)
-        
-    def on_timeout(self, event):
-        SplashScreen(self)
