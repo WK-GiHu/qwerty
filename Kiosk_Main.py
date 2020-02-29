11c87c7d8df031f347cf60f740753d45b0616c86
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-02-29 19:09:16 +0800

    Update Kiosk_Main.py

--- Kiosk_Main.py
+++ Kiosk_Main.py
@@ -35,6 +35,7 @@ class FingerprintThread(threading.Thread):
             retry = 0
             while True:
                 print('Waiting for finger...')
+                retry+=1
                 try:
                     while (f.readImage() == False):
                         pass
@@ -70,6 +71,93 @@ class FingerprintThread(threading.Thread):
                 else:
                     messagebox.showerror("Warning!","Your fingerprint is not yet registered!")
 
+    
+class RFIDThread(threading.Thread):
+    def __init__(self, app, callback):
+        super().__init__(daemon = True)
+        result = None
+        self.app = app
+        self.app.bind('<<GRANT_ACCESS>>', callback)
+        self.start()
+    
+    def run(self):
+        self.db = pymysql.connect(host = "192.168.1.19",port = 3306, user = "root",passwd = "justin",db= "thesis_db")
+        self.cursor = self.db.cursor()
+        self.db.autocommit(True)
+        self.reader = SimpleMFRC522()
+        while True:
+            self.id, text = self.reader.read()
+            self.cursor.execute("SELECT * FROM residents_admin WHERE RFID = %s",str(self.id))
+            RFIDThread.result = self.cursor.fetchone()
+            print(RFIDThread.result)
+            if RFIDThread.result: 
+                messagebox.showinfo("Success!", "Welcome")
+                self.app.event_generate('<<GRANT_ACCESS>>', state = 11, when='tail')
+            else:
+                self.cursor.execute("SELECT * FROM residents_db WHERE RFID = %s", str(self.id))
+                RFIDThread.result = self.cursor.fetchone()
+                print(RFIDThread.result)
+                if RFIDThread.result: 
+                    messagebox.showinfo("Success!", "Welcome")
+                    self.app.event_generate('<<GRANT_ACCESS>>', state = 12, when='tail')
+                else:
+                    messagebox.showerror("Warning!","Your RFID card is not yet registered!")
+
+
+class SplashScreen(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.attributes("-fullscreen", True)
+        self.funcid = self.bind('<Button-1>', self.on_button_clicked)
+        
+        
+        label = Label(self, text = "NO ANNOUNCEMENT YET!!!", bg = "white")
+        label.pack(fill = "both", expand =1)
+        
+        
+    def on_button_clicked(self, event):
+    
+        print('SplashScreen.on_button_click')
+        self.unbind('<Button-1>', funcid=self.funcid)
+        self.master.event_generate('<<IDLE>>', state = 1, when='tail')
+        self.destroy()
+         
+        
+class IdleCounter(threading.Thread):
+    label = 'idle'
+
+    def __init__(self, app):
+        super().__init__(daemon=True)
+        self.app = app
+
+        self.timeout = 20
+        self._counter = self.timeout
+
+        self.start()
+        self.app.bind('<<IDLE>>', self.on_idle)
+
+    def run(self):
+        print('run()'.format())
+        
+        while True:
+            if self._counter > 0:
+                self._counter -= 1
+                
+                if self._counter == 0:
+                    self.app.event_generate('<<TIMEOUT>>', when='tail')
+                    
+            time.sleep(1)
+                    
+        print('IdleCounter terminated'.format())
+        
+    def on_idle(self, event):
+        print('IdleCounter.on_idle(state={})'.format(event.state))
+        if event.state == 1:
+            self._counter = self.timeout
+        elif self._counter > 0 and event.state == 0:
+            self._counter = self.timeout
+
+    
 class Kiosk(tk.Tk):
     def __init__(self):
         super().__init__()
@@ -111,3 +199,55 @@ class Kiosk(tk.Tk):
         self.configure(bg="white")    
         self.geometry("{}x{}".format(self.ws, self.hs))
         self.T_printer = Usb(0x0fe6, 0x811e, 98, 0x82, 0x02)
+        
+    def on_timeout(self, event):
+        SplashScreen(self)
+             
+    def on_grant_access(self, event):
+        print('Kiosk.on_grant_access()')
+        if event.state <10:
+            self.details = FingerprintThread.result
+            self.firstn = self.details[1]
+            self.middlen = self.details[2]
+            self.lastn = self.details[3]
+            self.SEX = self.details[4]
+            self.dob = self.details[5]
+            self.civils = self.details[6]
+            self.year_resides = self.details[7]
+            self.address = self.details[8]
+            self.pob = self.details[9]
+            self.sq1 = self.details[10]
+            self.ans = self.details[11]
+            self.rf = self.details [12]
+            self.age = self.calculate_age(self.dob)
+            print(self.age)
+            print ("Finger Thread")
+            print (self.details)
+            if event.state == 1:
+                self.choose_admin()
+            elif event.state == 2:
+                self.choose_user()            
+        else:
+            self.details = RFIDThread.result
+            self.firstn = self.details[1]
+            self.middlen = self.details[2]
+            self.lastn = self.details[3]
+            self.SEX = self.details[4]
+            self.dob = self.details[5]
+            self.civils = self.details[6]
+            self.year_resides = self.details[7]
+            self.address = self.details[8]
+            self.pob = self.details[9]
+            self.sq1 = self.details[10]
+            self.ans = self.details[11]
+            self.rf = self.details [12]
+            self.age = self.calculate_age(self.dob)
+            print(self.age)
+            print ("RFID Thread")
+            if event.state == 11:
+                self.security_question()    
+            elif event.state == 12:
+                self.security_question()
+        
+        self.deiconify()
+            
