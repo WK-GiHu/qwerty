6b65e4b1b6a5acfb3fdddaaa1b2499073d3dc09c
WK-GiHu <WK-GiHu@users.noreply.github.com>
2020-03-14 00:48:16 +0100

    Add files via upload
    **Note**: Keep your current version, try only this version first before replacing with you current version

---
+++ FingerprintDevice.py
@@ -0,0 +1,112 @@
+from pyfingerprint.pyfingerprint import PyFingerprint
+import threading, time
+
+SEARCH = 1
+REGISTER = 2
+
+
+class FingerprintThread(threading.Thread):
+    MODE = SEARCH
+    template = None
+    
+    def __init__(self, app):
+        super().__init__(daemon=True)
+        
+        self.app = app
+        self.f = None
+        self._continue = threading.Event()
+        self.start()
+        
+    def delete_template(self, positionNumber):
+        # wait readImage
+        self._continue.clear()
+        time.sleep(0.5)
+        
+        b = self.f.deleteTemplate(positionNumber)
+        if b:
+            print('FingerprintThread:Template deleted!')
+            
+        # continue readImage
+        self._continue.set()
+        return b
+
+    def run(self):
+        try:
+            self.f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+            
+            if self.f.verifyPassword():
+                raise ValueError('The given fingerprint sensor password is wrong!')
+        
+        except Exception as e:
+            print('The fingerprint sensor could not be initialized!')
+            print('Exception message: ' + str(e))
+        
+        print('Currently used templates: ' + str(self.f.getTemplateCount()) + '/' + str(self.f.getStorageCapacity()))
+
+        # REGISTER sequence
+        sequence = 1
+        
+        while True:
+            retry = 0
+            while True:
+                print('Waiting for finger...')
+                retry += 1
+                try:
+                    while not self.f.readImage():
+                        # print('looping .readImage()')
+                        self._continue.wait()
+                        time.sleep(0.5)
+                    break
+                    
+                except Exception as e:
+                    print('PyFingerprint:{}, try {} of 3'.format(e, retry))
+                    if retry == 3:
+                        raise Exception('PyFingerprint: Failed to read image 3 times, exiting.')
+                    
+                    # delay 2 seconds before next try
+                    time.sleep(2)
+            # end while retry == 3
+            
+            if FingerprintThread.MODE == SEARCH:
+                self.f.convertImage(1)
+                FingerprintThread.template = self.f.searchTemplate()
+                self.f.app.event_generate('<<FINGERPRINT>>', when='tail', state=FingerprintThread.template[0])
+
+            elif FingerprintThread.MODE == REGISTER:
+                if sequence == 1:
+                    self.f.convertImage(1)
+                    FingerprintThread.template = self.f.searchTemplate()
+                    sequence += 1
+    
+                elif sequence == 2:
+                    self.f.convertImage(2)
+                    if self.f.compareCharacteristics():
+                        self.f.createTemplate()
+                        # ? characterics = str(self.f.downloadCharacteristics(0x01)).encode('utf-8')
+                        position_number = self.f.storeTemplate()
+                    else:
+                        position_number = -1
+
+                    self.f.app.event_generate('<<FINGERPRINT>>', when='tail', state=position_number)
+                    
+                    # Reset MODE
+                    sequence = 1
+                    FingerprintThread.MODE = SEARCH
+            
+            # Delay before next finger scan
+            time.sleep(2)
+
+
+if __name__ == "__main__":
+    import tkinter as tk
+    
+    
+    def on_fingerprint(event):
+        print('on_fingerprint()  positionNumber={}'.format(event.state))
+    
+    
+    root = tk.Tk()
+    FingerprintThread(root)
+    # FingerprintThread.MODE = REGISTER
+    root.bind('<<FINGERPRINT>>', on_fingerprint)
+    root.mainloop()
