1d61402cd1778d1e22235c971a1e4632c84863fb
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-03-13 19:14:45 +0800

    Update Kiosk_Main.py

--- Kiosk_Main.py
+++ Kiosk_Main.py
@@ -5,9 +5,10 @@ from mfrc522 import SimpleMFRC522
 import RPi.GPIO as GPIO
 from PIL import Image, ImageTk
 import datetime
-from pyfingerprint.pyfingerprint import PyFingerprint
+from FingerprintDevice import FingerprintThread
 from VKeyboard import VKeyboard
-class FingerprintThread(threading.Thread):
+
+class XFingerprintThread(threading.Thread):
     def __init__(self, app, callback):
         super().__init__(daemon = True)
         result = None
@@ -16,7 +17,7 @@ class FingerprintThread(threading.Thread):
         self.start()
 
     def run(self):        
-        self.db = pymysql.connect(host = "192.168.1.19",port = 3306, user = "root",passwd = "justin",db= "thesis_db")
+        self.db = pymysql.connect(host = "192.168.1.9",port = 3306, user = "root",passwd = "justin",db= "thesis_main")
         self.cursor = self.db.cursor()
         self.db.autocommit(True)
         try:
@@ -38,6 +39,7 @@ class FingerprintThread(threading.Thread):
                 retry+=1
                 try:
                     while (f.readImage() == False):
+                        print('looping .readImage()')
                         pass
                     break
                 except Exception as e:
@@ -81,7 +83,7 @@ class RFIDThread(threading.Thread):
         self.start()
     
     def run(self):
-        self.db = pymysql.connect(host = "192.168.1.19",port = 3306, user = "root",passwd = "justin",db= "thesis_db")
+        self.db = pymysql.connect(host = "192.168.1.9",port = 3306, user = "root",passwd = "justin",db= "thesis_main")
         self.cursor = self.db.cursor()
         self.db.autocommit(True)
         self.reader = SimpleMFRC522()
@@ -110,13 +112,10 @@ class SplashScreen(tk.Toplevel):
         self.attributes("-fullscreen", True)
         self.funcid = self.bind('<Button-1>', self.on_button_clicked)
         
-        
         label = Label(self, text = "NO ANNOUNCEMENT YET!!!", bg = "white")
         label.pack(fill = "both", expand =1)
         
-        
     def on_button_clicked(self, event):
-    
         print('SplashScreen.on_button_click')
         self.unbind('<Button-1>', funcid=self.funcid)
         self.master.event_generate('<<IDLE>>', state = 1, when='tail')
@@ -151,7 +150,6 @@ class IdleCounter(threading.Thread):
         print('IdleCounter terminated'.format())
         
     def on_idle(self, event):
-        print('IdleCounter.on_idle(state={})'.format(event.state))
         if event.state == 1:
             self._counter = self.timeout
         elif self._counter > 0 and event.state == 0:
@@ -162,11 +160,13 @@ class Kiosk(tk.Tk):
     def __init__(self):
         super().__init__()
         VKeyboard(self)
+        self.camb25 = font.Font(family='Cambria', size=25)
+        font.families()
         
-        self.db = pymysql.connect(host = "192.168.1.19",port = 3306, user = "root",passwd = "justin",db= "thesis_db")
+        self.db = pymysql.connect(host = "192.168.1.9",port = 3306, user = "root",passwd = "justin",db= "thesis_main")
         self.cursor = self.db.cursor()
         self.db.autocommit(True)
-
+        
         #create table
         """self.QueryResident = "CREATE TABLE IF NOT EXISTS residents_db (ID INT(11) not null AUTO_INCREMENT, FIRST_NAME varchar(255) not null, MIDDLE_NAME varchar(255) not null, LAST_NAME varchar(255) not null,SEX varchar(255) not null, BIRTH_DATE date, CIVIL_STATUS varchar(255) not null, YEAR_OF_RESIDENCY int, PLACE_OF_BIRTH varchar(255) not null, SECURITY_QUESTION varchar(255), ANSWER varchar(255), RFID varchar(255) not null, FINGER_TEMPLATE varchar(255), PRIMARY KEY (ID))"
         self.cursor.execute(self.QueryResident)
@@ -178,47 +178,71 @@ class Kiosk(tk.Tk):
         self.style = Style()
         self.style.theme_use('alt')
         self.style.map('TCombobox', fieldbackground=[('readonly','white')])
-                
+        
         self.ws = self.winfo_screenwidth()
         self.hs = self.winfo_screenheight()
         print(self.ws, self.hs)
         
         self.attributes("-fullscreen", True)
-        self.img = (Image.open("background_kiosk.png"))
+        self.img = (Image.open("login_bg.jpg"))
         self.image = self.img.resize((self.ws,self.hs))
         self.img_background = ImageTk.PhotoImage(self.image)
         self.imglabel = Label(self, image = self.img_background).place(x=0,y=0)
 
         self.img2 = ImageTk.PhotoImage(Image.open("rosario_logo.png"))
         
-        FingerprintThread(self, callback = self.on_grant_access)
+        XFingerprintThread(self, callback = self.on_grant_access)
+        self.bind('<<FINGERPRINT>>', self.on_fingerprint)
         RFIDThread(self, callback = self.on_grant_access)
         IdleCounter(self)
+        
         self.bind('<<TIMEOUT>>', self.on_timeout)
         GPIO.setwarnings(False)
         self.configure(bg="white")    
         self.geometry("{}x{}".format(self.ws, self.hs))
-        self.T_printer = Usb(0x0fe6, 0x811e, 98, 0x82, 0x02)
-        
+        #self.T_printer = Usb(0x0fe6, 0x811e, 98, 0x82, 0x02)
+    
+    def on_fingerprint(self, event):
+        positionNumber = result[0] 
+        accuracyScore = result[1] 
+      
+        self.cursor.execute("SELECT * FROM residents_admin WHERE FINGER_TEMPLATE = %s",positionNumber) 
+        FingerprintThread.result = self.cursor.fetchone() 
+        print (FingerprintThread.result) 
+        if event.result: 
+            event.state = 1  # or 
+            self.on_grant_access(event):
+        else: 
+            self.cursor.execute("SELECT * FROM residents_db WHERE FINGER_TEMPLATE = %s", positionNumber) 
+            event.result = self.cursor.fetchone()
+            print (FingerprintThread.result)
+            if FingerprintThread.result:
+                event.state = 2
+                self.on_grant_access(event):
+            else: 
+                messagebox.showerror("Warning!","Your fingerprint is not yet registered!")
+    
     def on_timeout(self, event):
         SplashScreen(self)
-             
+                  
     def on_grant_access(self, event):
         print('Kiosk.on_grant_access()')
         if event.state <10:
-            self.details = FingerprintThread.result
-            self.firstn = self.details[1]
+            self.details = event.result
+            self.firstn = self.details[3]
             self.middlen = self.details[2]
-            self.lastn = self.details[3]
+            self.lastn = self.details[1]
             self.SEX = self.details[4]
             self.dob = self.details[5]
             self.civils = self.details[6]
             self.year_resides = self.details[7]
             self.address = self.details[8]
             self.pob = self.details[9]
-            self.sq1 = self.details[10]
-            self.ans = self.details[11]
-            self.rf = self.details [12]
+            self.cn = self.details[10]
+            self.sq1 = self.details[11]
+            self.ans = self.details[12]
+            self.image = self.details[13]
+            self.rf = self.details [16]
             self.age = self.calculate_age(self.dob)
             print(self.age)
             print ("Finger Thread")
@@ -229,18 +253,20 @@ class Kiosk(tk.Tk):
                 self.choose_user()            
         else:
             self.details = RFIDThread.result
-            self.firstn = self.details[1]
+            self.firstn = self.details[3]
             self.middlen = self.details[2]
-            self.lastn = self.details[3]
+            self.lastn = self.details[1]
             self.SEX = self.details[4]
             self.dob = self.details[5]
             self.civils = self.details[6]
             self.year_resides = self.details[7]
             self.address = self.details[8]
             self.pob = self.details[9]
-            self.sq1 = self.details[10]
-            self.ans = self.details[11]
-            self.rf = self.details [12]
+            self.cn = self.details[10]
+            self.sq1 = self.details[11]
+            self.ans = self.details[12]
+            self.image = self.details[13]
+            self.rf = self.details [16]
             self.age = self.calculate_age(self.dob)
             print(self.age)
             print ("RFID Thread")
