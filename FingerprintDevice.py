e5974761d0b9401874fef569c4d65d91794a5b4f
Justinjay282 <59200116+Justinjay282@users.noreply.github.com>
2020-03-14 21:51:41 +0800

    Update FingerprintDevice.py

--- FingerprintDevice.py
+++ FingerprintDevice.py
@@ -4,7 +4,6 @@ import threading, time
 SEARCH = 1
 REGISTER = 2
 
-
 class FingerprintThread(threading.Thread):
     MODE = SEARCH
     template = None
@@ -15,7 +14,6 @@ class FingerprintThread(threading.Thread):
         self.app = app
         self.f = None
         self._continue = threading.Event()
-        
         self._continue.set()
         self.start()
         
@@ -27,7 +25,7 @@ class FingerprintThread(threading.Thread):
         b = self.f.deleteTemplate(positionNumber)
         if b:
             print('FingerprintThread:Template deleted!')
-            
+        
         # continue readImage
         self._continue.set()
         return b
@@ -64,7 +62,6 @@ class FingerprintThread(threading.Thread):
                     print('PyFingerprint:{}, try {} of 3'.format(e, retry))
                     if retry == 3:
                         raise Exception('PyFingerprint: Failed to read image 3 times, exiting.')
-                    
                     # delay 2 seconds before next try
                     time.sleep(2)
             # end while retry == 3
@@ -72,7 +69,7 @@ class FingerprintThread(threading.Thread):
             if FingerprintThread.MODE == SEARCH:
                 self.f.convertImage(1)
                 FingerprintThread.template = self.f.searchTemplate()
-                self.f.app.event_generate('<<FINGERPRINT>>', when='tail', state=FingerprintThread.template[0])
+                self.app.event_generate('<<FINGERPRINT>>', when='tail', state=FingerprintThread.template[0])
 
             elif FingerprintThread.MODE == REGISTER:
                 if sequence == 1:
@@ -89,7 +86,7 @@ class FingerprintThread(threading.Thread):
                     else:
                         position_number = -1
 
-                    self.f.app.event_generate('<<FINGERPRINT>>', when='tail', state=position_number)
+                    self.app.event_generate('<<FINGERPRINT>>', when='tail', state = position_number)
                     
                     # Reset MODE
                     sequence = 1
@@ -106,7 +103,8 @@ if __name__ == "__main__":
     def on_fingerprint(event):
         print('on_fingerprint()  positionNumber={}'.format(event.state))
     
-    if 0:  # test MODE REGISTER
+    if 1:  # test MODE REGISTER
+        print('on_register')
         FingerprintThread.MODE = REGISTER
     
     root = tk.Tk()
